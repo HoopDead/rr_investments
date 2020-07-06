3576d4c98d2dcd459fe083e56fd667040716e3a6
Kamil <kkowalczyk848@gmail.com>
2020-07-06 11:48:39 +0200

    Update contact.jsx

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 
-import { MDBBtn, MDBCol, MDBContainer, MDBRow } from "mdbreact";
+import { MDBBtn, MDBCol, MDBContainer, MDBRow, MDBInput } from "mdbreact";
 
 class Contact extends Component {
   render() {
     return (
       <MDBContainer>
-        <MDBRow center style={{ height: "100vh" }}>
-          <MDBCol middle="true" sm="8" className="text-center">
-            <h1>Contact</h1>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
+         <MDBRow center style = {{height: "100vh"}}>
+            <h1 style = {{fontSize: 26, height: 50}}>Zrobimy dla Ciebie cos wspanialego, ale najpierw <span style = {{color: "coral"}}>porozmawiajmy.</span></h1>
+            <form>
+            <p className="h5 text-center mb-4">Wypełnij poniższy formularz!</p>
+            <div className="grey-text">
+           <MDBInput label="Imie"  name = "name"/>
+           <MDBInput label="Email"  name = "email"/>
+           <MDBInput label="Nazwa firmy" name = "text" />
+           <MDBInput label="Numer telefonu" name = "tel"/>
+           </div>
+           <div className="text-center">
+           <MDBBtn color = "orange">Wyślij</MDBBtn>
+           </div>
+           </form>
+          </MDBRow>
+       </MDBContainer>
     );
   }
 }
