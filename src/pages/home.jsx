e1936cfa966ede6d9abf3781b36250f94087a836
Granddd <granddw4@gmail.com>
2020-07-06 19:49:01 +0200

    Fixed all problems with home page layout

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,12 +7,14 @@ import { MDBBtn, MDBCol, MDBRow } from "mdbreact";
 class Home extends Component {
   render() {
     return (
-        <MDBRow center>
-          <MDBCol lg="6">
-              <img src={Landing} alt="Landing" className="img-fluid"/>
+        <MDBRow center className="d-flex align-items-center">
+          <MDBCol md="6" className="order-md-2 m-2">
+          <div className="view overlay">
+            <img src={Landing} alt="Logo" className="img-fluid"/>
+          </div>
           </MDBCol>
-          <MDBCol lg="4" className="text-center align-bottom pt-5">
-              <p className="h2">R&R Company</p>
+          <MDBCol md="3" className="order-md-1  text-center m-2">
+              <p className="h1">R&R Company</p>
               <hr/>
               <p>You have an idea? You want to develop and create a brand new product? Get in touch with us and we will change the future togheter!</p>
               <a href="https://mdbootstrap.com/" class="btn btn-deep-orange">CONTACT US</a>
