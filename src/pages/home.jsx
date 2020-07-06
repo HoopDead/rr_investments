37e1ab34c97e9cb5ab1caaea801312f817564e66
Granddd <granddw4@gmail.com>
2020-07-06 14:08:33 +0200

    Home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,25 +1,21 @@
 import React, { Component } from "react";
 
-<<<<<<< HEAD
-import Landing from "../assets/landing.png"
-=======
 import Landing from "../assets/images/landing.png"
->>>>>>> bf50a890403356fb98e227e4013424bca9efa95f
 
 import { MDBBtn, MDBCol, MDBRow } from "mdbreact";
 
 class Home extends Component {
   render() {
     return (
-        <MDBRow center style={{ height: "100vh" }}>
-          <MDBCol md="7" className="view overlay">
-            <img src={Landing} alt="Landing" className="card-img-top"/>
+        <MDBRow center>
+          <MDBCol lg="6">
+              <img src={Landing} alt="Landing" className="img-fluid"/>
           </MDBCol>
-          <MDBCol md="5">
-            <div>
+          <MDBCol lg="4" className="text-center align-bottom pt-5">
               <p className="h2">R&R Company</p>
+              <hr/>
               <p>You have an idea? You want to develop and create a brand new product? Get in touch with us and we will change the future togheter!</p>
-            </div>
+              <a href="https://mdbootstrap.com/" class="btn btn-deep-orange">CONTACT US</a>
           </MDBCol>
         </MDBRow>
     );
