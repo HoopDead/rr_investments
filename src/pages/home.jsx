085068c95896cfa54f24062d2842907e049b0b86
Granddd <granddw4@gmail.com>
2020-07-06 12:11:24 +0200

    cos

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
 
-import Landing from "../assets/landing.svg"
+import Landing from "../assets/landing.png"
 
-import { MDBBtn, MDBCol, MDBContainer, MDBRow } from "mdbreact";
+import { MDBBtn, MDBCol, MDBRow } from "mdbreact";
 
 class Home extends Component {
   render() {
     return (
-      <MDBContainer>
         <MDBRow center style={{ height: "100vh" }}>
-          <MDBCol md="5" sm="8" className="text-center h-50">
-            <img src={Landing} alt="Landing"/>
+          <MDBCol md="7" className="view overlay">
+            <img src={Landing} alt="Landing" className="card-img-top"/>
+          </MDBCol>
+          <MDBCol md="5">
+            <div>
+              <p className="h2">R&R Company</p>
+              <p>You have an idea? You want to develop and create a brand new product? Get in touch with us and we will change the future togheter!</p>
+            </div>
           </MDBCol>
         </MDBRow>
-      </MDBContainer>
     );
   }
 }
