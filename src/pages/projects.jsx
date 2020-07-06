b43794bbdfa949c623c6993a57a1901d47a8fd11
HoopDead <polec0@wp.pl>
2020-07-06 11:28:25 +0200

    Export from svg to png

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -5,13 +5,11 @@ import { MDBBtn, MDBCol, MDBContainer, MDBRow } from "mdbreact";
 class Projects extends Component {
   render() {
     return (
-      <MDBContainer>
         <MDBRow center style={{ height: "100vh" }}>
           <MDBCol middle="true" sm="8" className="text-center">
             <h1>Projects</h1>
           </MDBCol>
         </MDBRow>
-      </MDBContainer>
     );
   }
 }
