a52671b8953f71878ad368e26a4d16c497e0a492
Granddd <granddw4@gmail.com>
2020-07-07 18:49:09 +0200

    Navbar hover effect

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 import { Router } from "@reach/router";
 
 /* Here we imnport assets */
-import { MDBContainer } from "mdbreact";
 import "./index.css";
 /* here we import pages or components */
 
--- src/components/navbar-bot.jsx
+++ src/components/navbar-bot.jsx
@@ -7,21 +7,29 @@ function NavBot() {
     return (
         <MDBNavbar expand className="d-md-none d-flex fixed-bottom">
         <MDBNavbarNav className="text-uppercase justify-content-center text-center">
-            <MDBNavItem className="mr-2 d-flex flex-column text-center">
-                <MDBIcon icon="home" size="lg"/>
-                <a href="przyklad">start</a>
+            <MDBNavItem className="mr-2">
+                <a href="przyklad1" className="d-flex flex-column">
+                    <MDBIcon icon="home" size="lg"/>
+                    <span>start</span>
+                </a>
             </MDBNavItem>
-            <MDBNavItem active className="mr-2 d-flex flex-column">
-                <MDBIcon icon="users" size="lg"/>
-                <a href="przyklad2">About Us</a>
+            <MDBNavItem active className="mr-2">
+                <a href="przyklad2" className="d-flex flex-column">
+                    <MDBIcon icon="users" size="lg"/>
+                    <span>About Us</span>
+                </a>
             </MDBNavItem>
             <MDBNavItem className="mr-2 d-flex flex-column">
-                <MDBIcon icon="suitcase" size="lg"/>
-                <a href="przyklad3">Projects</a>
+                <a href="przyklad3" className="d-flex flex-column">
+                    <MDBIcon icon="suitcase" size="lg"/>
+                    <span>Projects</span>
+                </a>
             </MDBNavItem>
             <MDBNavItem className="mr-2 d-flex flex-column">
-                <MDBIcon icon="suitcase" size="lg"/>
-                <a href="przyklad4">Contact</a>
+                <a href="przyklad4" className="d-flex flex-column">
+                    <MDBIcon icon="envelope" size="lg"/>
+                    <span>Contact</span>
+                </a>
             </MDBNavItem>
         </MDBNavbarNav>
     </MDBNavbar>
