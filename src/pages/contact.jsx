22ca6d6e7591b90520a576e3bf4cc4dd5373abf4
Kamil <kkowalczyk848@gmail.com>
2020-07-07 11:26:35 +0200

    update contact.jsx

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,30 +2,55 @@ import React, { Component } from "react";
 
 import { MDBBtn, MDBCol, MDBContainer, MDBRow, MDBInput } from "mdbreact";
 
+import Envelope from "../assets/images/projects_envelope.png";
+import Telephone from "../assets/images/projects_telephone.png";
+import Mess from "../assets/images/projects_mess.png";
+
+
 class Contact extends Component {
   render() {
     return (
-      <MDBContainer>
+      <MDBContainer fluid >
          <MDBRow center>
-            <h1 style = {{fontSize: 30, height: 50}}>Zrobimy dla Ciebie coś wspaniałego, ale najpierw <span style = {{color: "coral"}}>porozmawiajmy.</span></h1>
-            <form style = {{marginTop: 50}}>
-            <p className="h5 text-center mb-4">Wypełnij poniższy formularz!</p>
-            <div className="grey-text">
-             <MDBRow>
-               <MDBInput className = "col-sm-6" label="Imie"  name = "name"/>
-               <MDBInput  className = "col-sm-6"label="Email"  name = "email"/>
-             </MDBRow>
-             <MDBRow>
-               <MDBInput label="Nazwa firmy" name = "text" />
-               <MDBInput label="Numer telefonu" name = "tel"/>
-             </MDBRow>
-           </div>
-           <div className="text-center">
-           <MDBBtn rounded >Wyślij</MDBBtn>
-           </div>
-           </form>
+         <p className="h4 text-center mb-4">Zrobimy dla Ciebie coś wspaniałego, ale najpierw <span style = {{color: "coral"}}>porozmawiajmy.</span></p>
           </MDBRow>
-       </MDBContainer>
+          <MDBRow center>
+            <form style = {{marginTop: 75}}>
+                <p className="h5 text-center mb-4">Wypełnij poniższy formularz!</p>
+                <div className="grey-text">
+                 <MDBRow>
+                    <MDBInput className = "col-sm-12" label="Imie"  name = "name"/>
+                    <MDBInput  className = "col-sm-12" label="Email"  name = "email"/>
+                 </MDBRow>
+                 <MDBRow>
+                    <MDBInput className = "col-sm-12" label="Nazwa firmy" name = "text" />
+                    <MDBInput className = "col-sm-12" label="Numer telefonu" name = "tel"/>
+                 </MDBRow>
+                </div>
+                <div className="text-center">
+                  <MDBBtn>Zapisz się!</MDBBtn>
+                </div>
+             </form>
+             </MDBRow>
+             <div style = {{backgroundColor: "coral", width: "100vw", height: "5px", marginTop: "20px", marginBottom: "20px"}}></div>
+             <p className = "h4 text-center mb-4">Wolisz tradycyjne metody?</p>
+          <MDBRow>
+            <MDBCol className = "col-sm-4 text-center">
+               <img src = {Envelope} className = "img-fluid w-20"></img>
+               <p>lorem@ipsum.com</p>
+            </MDBCol>
+            <MDBCol className = "col-sm-4 text-center">
+               <img src = {Mess} className = "img-fluid w-20"></img>
+               <p>RRInvestments</p>
+            </MDBCol>
+            <MDBCol className = "col-sm-4 text-center">
+               <img src = {Telephone} className = "img-fluid w-20"></img>
+               <p>+48 12456789</p>
+            </MDBCol>
+            </MDBRow>
+          <div style = {{backgroundColor: "coral", width: "100vw", height: "5px", marginTop: "20px", marginBottom: "20px"}}></div>
+          <p className = "h5 text-center mb-4">Czekamy na twoją wiadomość!</p>
+      </MDBContainer>
     );
   }
 }
