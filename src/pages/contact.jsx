f2095d1c1eafa8dca9396773bc38f9f839856e8d
HoopDead <polec0@wp.pl>
2020-07-07 15:21:43 +0200

    Beautify on contact.jsx to layout on figma

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { MDBBtn, MDBCol, MDBContainer, MDBRow, MDBInput } from "mdbreact";
+import { MDBBtn, MDBCol, MDBContainer, MDBRow, MDBInput, MDBIcon } from "mdbreact";
 
 import Envelope from "../assets/images/projects_envelope.png";
 import Telephone from "../assets/images/projects_telephone.png";
@@ -13,46 +13,46 @@ class Contact extends Component {
     return (
       <MDBContainer fluid>
          <MDBRow center>
-         <p style = {{marginTop: "50px"}} className="h4 text-center mb-4">Zrobimy dla Ciebie coś wspaniałego, ale najpierw <span style = {{color: "coral"}}>porozmawiajmy.</span></p>
+             <p className="h4 text-center mb-4 mt-5 py-2 font-raleway-bold">Zrobimy dla Ciebie coś wspaniałego, ale najpierw <span style = {{color: "coral"}}>porozmawiajmy.</span></p>
           </MDBRow>
           <MDBRow center>
-            <form style = {{marginTop: 75}}>
-                <p className="h5 text-center mb-4">Wypełnij poniższy formularz!</p>
+            <form>
+                <p className="h6 text-center mb-4">Wypełnij poniższy formularz!</p>
                 <div className="grey-text">
                    <MDBRow center>
-                      <MDBInput  style = {{width: "300px"}} className = "col-md-12" label="Imie"  name = "name"/>
+                      <MDBInput  style = {{width: "250px"}} className = "col-md-12" label="Imie"  name = "name"/>
                       <span className = "d-none d-lg-flex" style={{width:"30px"}}></span> 
-                      <MDBInput style = {{width: "300px"}} className = "col-md-12" label="Email"  name = "email"/>  
+                      <MDBInput style = {{width: "250px"}} className = "col-md-12" label="Email"  name = "email"/>  
                    </MDBRow>
                    <MDBRow center> 
-                      <MDBInput style = {{width: "300px"}} className = "col-md-12" label="Nazwa firmy" name = "text" />
+                      <MDBInput style = {{width: "250px"}} className = "col-md-12" label="Nazwa firmy" name = "text" />
                       <span className = "d-none d-lg-flex" style={{width:"30px"}}></span>
-                      <MDBInput style = {{width: "300px"}} className = "col-md-12" label="Numer telefonu" name = "tel"/>
+                      <MDBInput style = {{width: "250px"}} className = "col-md-12" label="Numer telefonu" name = "tel"/>
                    </MDBRow>
                 </div>
                 <div className="text-center">
-                  <MDBBtn floating gradient="peach">Zapisz się!</MDBBtn>
+                  <button className = "btn contact-button-orange w-50 my-4">Zapisz się!</button>
                 </div>
              </form>
              </MDBRow>
-             <div style = {{backgroundColor: "coral", height: "5px", marginTop: "60px", marginBottom: "20px"}}></div>
-             <p className = "h5 text-center mb-4">Wolisz tradycyjne metody?</p>
+             <hr className = "contact-hr-orange"></hr>
+             <p className = "h5 text-center my-4">Wolisz tradycyjne metody?</p>
           <MDBRow>
-             <MDBCol className = "col-sm-4 text-center">
+             <MDBCol className = "col-sm-4 text-center my-2">
                <img src = {Envelope} className = "img-fluid w-20"></img>
-               <p>lorem@ipsum.com</p>
+               <p className = "mt-1"><span class = "font-raleway-bold">lorem</span>@ipsum.com</p>
              </MDBCol>
-             <MDBCol className = "col-sm-4 text-center">
+             <MDBCol className = "col-sm-4 text-center my-2">
                <img src = {Mess} className = "img-fluid w-20"></img>
-               <p>RRInvestments</p>
+               <p className = "mt-1">RR <span class = "font-raleway-bold">Investments</span></p>
              </MDBCol>
-             <MDBCol className = "col-sm-4 text-center">
+             <MDBCol className = "col-sm-4 text-center my-2">
                <img src = {Telephone} className = "img-fluid w-20"></img>
-               <p>+48 12456789</p>
+               <p className = "mt-1"><span className = "font-raleway-bold">+48</span> 124 567 289</p>
              </MDBCol>
             </MDBRow>
-           <div style = {{backgroundColor: "coral", height: "5px", marginTop: "20px", marginBottom: "20px"}}></div>
-           <p className = "h5 text-center mb-4">Czekamy na twoją wiadomość!</p>
+            <hr className = "contact-hr-orange"></hr>
+           <p className = "font-raleway-bold h5 text-center my-4">Czekamy na twoją wiadomość!<MDBIcon className = "contact-icon mx-2" icon = "heart"></MDBIcon></p>
       </MDBContainer>
     );
   }
