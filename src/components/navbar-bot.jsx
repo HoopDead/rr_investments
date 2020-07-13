bebd59281b702b4c65df6035ccf6e879b07507b8
Kamil <kkowalczyk848@gmail.com>
2020-07-13 11:57:48 +0200

    highlighting icons in bottom navbar

--- src/components/navbar-bot.jsx
+++ src/components/navbar-bot.jsx
@@ -28,13 +28,25 @@ class NavBot extends Component {
         console.log(homeHeight, aboutHeight, projectsHeight, contactHeight)
 
         if(scrollTop < aboutHeight) {
-            console.log("Im on home");
+            document.getElementById("home-icon").style.color = "coral";
+            document.getElementById("about-icon").style.color = "";
+            document.getElementById("projects-icon").style.color = "";
+            document.getElementById("contact-icon").style.color = "";
         } else if (scrollTop > aboutHeight && scrollTop < projectsHeight) {
-            console.log("Im on about");
+            document.getElementById("home-icon").style.color = "";
+            document.getElementById("about-icon").style.color = "coral";
+            document.getElementById("projects-icon").style.color = "";
+            document.getElementById("contact-icon").style.color = "";
         } else if(scrollTop > projectsHeight && scrollTop < contactHeight) {
-            console.log("Im on projects");
+            document.getElementById("home-icon").style.color = "";
+            document.getElementById("about-icon").style.color = "";
+            document.getElementById("projects-icon").style.color = "coral";
+            document.getElementById("contact-icon").style.color = "";
         } else {
-            console.log("Im on contact");
+            document.getElementById("home-icon").style.color = "";
+            document.getElementById("about-icon").style.color = "";
+            document.getElementById("projects-icon").style.color = "";
+            document.getElementById("contact-icon").style.color = "coral";
         }
 
         this.setState({
@@ -53,25 +65,25 @@ class NavBot extends Component {
             <MDBNavbarNav className="text-uppercase justify-content-center text-center py-1">
                 <MDBNavItem className="mr-2 mt-1">
                     <a href="#home" className="d-flex flex-column">
-                        <MDBIcon icon="home" size="lg"/>
+                        <MDBIcon id="home-icon" icon="home" size="lg"/>
                         <span>start</span>
                     </a>
                 </MDBNavItem>
                 <MDBNavItem active className="mr-2 mt-1">
                     <a href="#about" className="d-flex flex-column">
-                        <MDBIcon icon="users" size="lg"/>
+                        <MDBIcon id="about-icon" icon="users" size="lg"/>
                         <span>About Us</span>
                     </a>
                 </MDBNavItem>
                 <MDBNavItem className="mr-2 d-flex flex-column mt-1">
                     <a href="#projects-section" className="d-flex flex-column">
-                        <MDBIcon icon="suitcase" size="lg"/>
+                        <MDBIcon id="projects-icon" icon="suitcase" size="lg"/>
                         <span>Projects</span>
                     </a>
                 </MDBNavItem>
                 <MDBNavItem className="mr-2 d-flex flex-column mt-1">
                     <a href="#contact" className="d-flex flex-column">
-                        <MDBIcon icon="envelope" size="lg"/>
+                        <MDBIcon id="contact-icon" icon="envelope" size="lg"/>
                         <span>Contact</span>
                     </a>
                 </MDBNavItem>
