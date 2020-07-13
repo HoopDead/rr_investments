2f5b5849246bd6765f38d722ef9bf3d3434f9f35
HoopDead <polec0@wp.pl>
2020-07-13 12:12:30 +0200

    Icon active when scrolling

--- src/components/navbar-bot.jsx
+++ src/components/navbar-bot.jsx
@@ -7,6 +7,13 @@ class NavBot extends Component {
         scrollTop: 0
     }
 
+
+    windowOnLoadOverride() {
+        window.onload = function() {
+            document.getElementById("home-icon").style.color = "coral";
+        }
+    }
+
     listenToScrollEvent() {
         document.addEventListener("scroll", () => {
             requestAnimationFrame(() => {
@@ -56,6 +63,7 @@ class NavBot extends Component {
 
 
     componentDidMount() {
+        this.windowOnLoadOverride()
         this.listenToScrollEvent()
     }
     
@@ -64,26 +72,26 @@ class NavBot extends Component {
             <MDBNavbar expand className="d-md-none d-flex fixed-bottom white">
             <MDBNavbarNav className="text-uppercase justify-content-center text-center py-1">
                 <MDBNavItem className="mr-2 mt-1">
-                    <a href="#home" className="d-flex flex-column">
-                        <MDBIcon id="home-icon" icon="home" size="lg"/>
+                    <a href="#home" id="home-icon" className="d-flex flex-column">
+                        <MDBIcon icon="home" size="lg"/>
                         <span>start</span>
                     </a>
                 </MDBNavItem>
                 <MDBNavItem active className="mr-2 mt-1">
-                    <a href="#about" className="d-flex flex-column">
-                        <MDBIcon id="about-icon" icon="users" size="lg"/>
+                    <a href="#about" id="about-icon" className="d-flex flex-column">
+                        <MDBIcon icon="users" size="lg"/>
                         <span>About Us</span>
                     </a>
                 </MDBNavItem>
                 <MDBNavItem className="mr-2 d-flex flex-column mt-1">
-                    <a href="#projects-section" className="d-flex flex-column">
-                        <MDBIcon id="projects-icon" icon="suitcase" size="lg"/>
+                    <a href="#projects-section" id="projects-icon" className="d-flex flex-column">
+                        <MDBIcon icon="suitcase" size="lg"/>
                         <span>Projects</span>
                     </a>
                 </MDBNavItem>
                 <MDBNavItem className="mr-2 d-flex flex-column mt-1">
-                    <a href="#contact" className="d-flex flex-column">
-                        <MDBIcon id="contact-icon" icon="envelope" size="lg"/>
+                    <a href="#contact" id="contact-icon" className="d-flex flex-column">
+                        <MDBIcon icon="envelope" size="lg"/>
                         <span>Contact</span>
                     </a>
                 </MDBNavItem>
