c420616953e6f7aca686efd7c3713eb7b4bfb070
HoopDead <polec0@wp.pl>
2020-07-25 11:11:04 +0200

    Updated Miss Dress logo

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import { MDBCol, MDBContainer, MDBRow } from "mdbreact";
 
 import MissDress from "../assets/images/missdress.png";
-import SecondProject from "../assets/images/projects_second.png";
+import Strzyga from "../assets/images/strzyga.png";
 import ThirdProject from "../assets/images/projects_third.png";
 
 class Projects extends Component {
@@ -17,7 +17,7 @@ class Projects extends Component {
               <p className = "text-justify my-4 mx-auto text-w-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at sem aliquet, aliquet <span className = "font-raleway-bold">libreo</span> quis, vulputate ligula. Suspendisse non volutpat enim, quis euismod Morbi ornare, lorem interdum congue interdum, magna <span className = "font-raleway-bold">ligula</span> consequat lectus, eget iaculis ligula magna id lacus. Fusce blandit nunc ac aliquet cursus. Integer tempus nisl et est eleifend </p>
             </MDBCol>
             <MDBCol lg = "4" xs = "12" className = "mt-5 py-5 text-center">
-              <img className = "img-fluid" alt="Second Project Logo" src = {SecondProject}></img>
+              <img className = "img-fluid" alt="Second Project Logo" src = {Strzyga}></img>
               <h3 className = "mt-5 font-raleway-bold text-center">Tytuł strony #2</h3>
               <p className = "text-justify my-4 mx-auto text-w-auto">Lorem ipsum dolor sit amet, consectetur <span className = "font-raleway-bold">adipiscing</span> elit. Etiam at sem aliquet, aliquet libero quis, vulputate ligula. Suspendisse non volutpat enim, quis euismod Morbi ornare, <span className = "font-raleway-bold">lorem</span> interdum congue interdum, magna ligula consequat lectus, eget iaculis ligula magna id lacus. Fusce blandit nunc ac aliquet cursus. Integer tempus nisl et est eleifend </p>
             </MDBCol>
