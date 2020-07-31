b8c5172853c2bfe1a314c9e165f3b057748535a2
HoopDead <polec0@wp.pl>
2020-07-31 21:59:17 +0200

    Updated project details loop from index lenght iteration to item iteration

--- src/pages/project_detail.jsx
+++ src/pages/project_detail.jsx
@@ -4,21 +4,20 @@ import { MDBCol, MDBContainer, MDBRow } from "mdbreact";
 import projects from "../assets/projects.json";
 
 function ProjectDetail({ match }) {
-    console.log(match);
-    for (let i = 0; i < projects.length; i++) {
-        if (projects[i].slug === match.params.slug) {
+    for (var project of projects) {
+        if(project.slug === match.params.slug) {
             return (
                 <MDBContainer>
                     <MDBRow>
                         <MDBCol md="12">
-                            <p>{projects[i].title}</p>
-                            <img className="w-100" src={require('../assets/images' + projects[i].previewPhoto)} alt="sadas" />
+                            <p>{project.title}</p>
+                            <img className="w-100" src={require('../assets/images' + project.previewPhoto)} alt="sadas" />
                         </MDBCol>
                     </MDBRow>
                 </MDBContainer>
-            )
+                )
+            }
         }
-    }
 }
 
 export default ProjectDetail;
