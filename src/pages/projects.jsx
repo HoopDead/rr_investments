5ef8b4024b98aff443582c5ca551c59468642b0f
HoopDead <polec0@wp.pl>
2020-08-03 15:53:56 +0200

    Working on responsive design on photos

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -8,16 +8,16 @@ class Projects extends Component {
   render() {
     return (
       <MDBContainer fluid id="projects-section" className="min-vh-100">
+          <h3 className = "mx-auto text-center"><span className = "font-raleway-bold">Wybrane realizacje</span></h3>
+          <hr className = "w-25 text-center projects-hr"></hr>
         <MDBRow>
           {projects.map((project, index) => {
             if (index >= 3) return null;
 
             return (
-              <MDBCol lg="4" xs="12" className="mt-5 py-5 text-center" key={project.slug}>
+              <MDBCol lg="4" xs="12" className="mt-2 py-5 text-center" key={project.slug}>
                 <Link to={`/projekt/${project.slug}`}>
-                  <img className="img-fluid" alt="First project logo" src={require("../assets/images/" + project.previewPhoto)}></img>
-                  <h3 className="mt-5 font-raleway-bold text-center black-text">{project.title}</h3>
-                  <p className="text-justify my-4 mx-auto text-w-auto black-text">{project.description}</p>
+                  <img alt="First project logo" src={require("../assets/images/" + project.previewPhoto)}></img>
                 </Link>
               </MDBCol>
             )
